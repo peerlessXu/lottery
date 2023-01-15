1c63a26d1c0a1a5a808d1dde1d0387d976885add
xuyongfei <xuyongfei@dingdingzn.com>
2023-01-15 11:02:03 +0800

    feat: 修复获奖名单；新增抽奖名单权重

---
+++ js/member.js
@@ -0,0 +1,398 @@
+var member = [
+  {
+    "phone": "No.3855",
+    "name": "四代猫愿",
+    "weight": 500
+  },
+  {
+    "phone": "No.6389",
+    "name": "人形大魔王",
+    "weight": 300
+  },
+  {
+    "phone": "No.4440",
+    "name": "痴言心醉",
+    "weight": 100
+  },
+  {
+    "phone": "No.3170",
+    "name": "渣瓜一隻",
+    "weight": 50
+  },
+  {
+    "phone": "No.1688",
+    "name": "住之江圭太"
+  },
+  {
+    "phone": "No.0018",
+    "name": "九条鱼卡"
+  },
+  {
+    "phone": "No.0318",
+    "name": "nightor"
+  },
+  {
+    "phone": "No.7712",
+    "name": "井下落石"
+  },
+  {
+    "phone": "No.6561",
+    "name": "埃尔o妮娅"
+  },
+  {
+    "phone": "No.1260",
+    "name": "Sapha"
+  },
+  {
+    "phone": "No.6280",
+    "name": "西行寺蓝蓝路"
+  },
+  {
+    "phone": "No.8517",
+    "name": "堀江由衣"
+  },
+  {
+    "phone": "No.2335",
+    "name": "十万巫女"
+  },
+  {
+    "phone": "No.2681",
+    "name": "Halu"
+  },
+  {
+    "phone": "No.4024",
+    "name": "lupin"
+  },
+  {
+    "phone": "No.0463",
+    "name": "太阳主宰"
+  },
+  {
+    "phone": "No.2423",
+    "name": "alkd"
+  },
+  {
+    "phone": "No.5357",
+    "name": "Allenz"
+  },
+  {
+    "phone": "No.8751",
+    "name": "达也的背影"
+  },
+  {
+    "phone": "No.0737",
+    "name": "神隐少女"
+  },
+  {
+    "phone": "No.5959",
+    "name": "尐疯寳児不尐了"
+  },
+  {
+    "phone": "No.8909",
+    "name": "⌒袶醽之子↓"
+  },
+  {
+    "phone": "No.8441",
+    "name": "我家的二喵"
+  },
+  {
+    "phone": "No.1746",
+    "name": "维他酱酱"
+  },
+  {
+    "phone": "No.2856",
+    "name": "dodo"
+  },
+  {
+    "phone": "No.6703",
+    "name": "米哟哟的咧"
+  },
+  {
+    "phone": "No.9092",
+    "name": "一根呆毛"
+  },
+  {
+    "phone": "No.3539",
+    "name": "Nadia"
+  },
+  {
+    "phone": "No.7180",
+    "name": "hunter"
+  },
+  {
+    "phone": "No.3679",
+    "name": "名将FG"
+  },
+  {
+    "phone": "No.5131",
+    "name": "线性近似"
+  },
+  {
+    "phone": "No.4146",
+    "name": "LOCKOFF"
+  },
+  {
+    "phone": "No.2812",
+    "name": "jessie"
+  },
+  {
+    "phone": "No.6918",
+    "name": "unoo"
+  },
+  {
+    "phone": "No.3776",
+    "name": "上升气流君"
+  },
+  {
+    "phone": "No.9873",
+    "name": "匿名希望"
+  },
+  {
+    "phone": "No.1240",
+    "name": "下载好慢"
+  },
+  {
+    "phone": "No.4839",
+    "name": "敢来一炮么"
+  },
+  {
+    "phone": "No.0832",
+    "name": "高町奈叶"
+  },
+  {
+    "phone": "No.5205",
+    "name": "我来打酱油"
+  },
+  {
+    "phone": "No.4787",
+    "name": "JackDee"
+  },
+  {
+    "phone": "No.2957",
+    "name": "加藤雪冬"
+  },
+  {
+    "phone": "No.1294",
+    "name": "轻抿一口菊花茶"
+  },
+  {
+    "phone": "No.2104",
+    "name": "捕猎Xloli"
+  },
+  {
+    "phone": "No.3113",
+    "name": "南山有瓜"
+  },
+  {
+    "phone": "No.8897",
+    "name": "きょうし"
+  },
+  {
+    "phone": "No.1823",
+    "name": "50cent"
+  },
+  {
+    "phone": "No.4174",
+    "name": "菊花交出来"
+  },
+  {
+    "phone": "No.2189",
+    "name": "Fire小殳"
+  },
+  {
+    "phone": "No.1691",
+    "name": "漆黑の斗狼"
+  },
+  {
+    "phone": "No.9511",
+    "name": "远野志贵"
+  },
+  {
+    "phone": "No.1513",
+    "name": "12週期新月"
+  },
+  {
+    "phone": "No.1881",
+    "name": "狂气的月的兔"
+  },
+  {
+    "phone": "No.8433",
+    "name": "田村ゆかり"
+  },
+  {
+    "phone": "No.4468",
+    "name": "右代宫战人"
+  },
+  {
+    "phone": "No.2172",
+    "name": "藤原妹紅"
+  },
+  {
+    "phone": "No.1038",
+    "name": "萌萌自由"
+  },
+  {
+    "phone": "No.8794",
+    "name": "八宝五圆杀"
+  },
+  {
+    "phone": "No.0869",
+    "name": "Raryooh"
+  },
+  {
+    "phone": "No.6173",
+    "name": "3y3s"
+  },
+  {
+    "phone": "No.0857",
+    "name": "长门有妻"
+  },
+  {
+    "phone": "No.3128",
+    "name": "装满酱油的好船"
+  },
+  {
+    "phone": "No.9433",
+    "name": "超魔理沙"
+  },
+  {
+    "phone": "No.5573",
+    "name": "灯灯灯等"
+  },
+  {
+    "phone": "No.6059",
+    "name": "回lao家结婚"
+  },
+  {
+    "phone": "No.9924",
+    "name": "奥妮克西娅"
+  },
+  {
+    "phone": "No.7851",
+    "name": "道羝王者"
+  },
+  {
+    "phone": "No.3350",
+    "name": "加納佳代子"
+  },
+  {
+    "phone": "No.0520",
+    "name": "K - MAID"
+  },
+  {
+    "phone": "No.2617",
+    "name": "山寨荼荼丸1"
+  },
+  {
+    "phone": "No.7819",
+    "name": "墙角渣渣颓废子"
+  },
+  {
+    "phone": "No.1714",
+    "name": "心理医生"
+  },
+  {
+    "phone": "No.8236",
+    "name": "加纳佳代子"
+  },
+  {
+    "phone": "No.8025",
+    "name": "库里艾拉"
+  },
+  {
+    "phone": "No.9004",
+    "name": "手只是装饰"
+  },
+  {
+    "phone": "No.9650",
+    "name": "秋庭里香酱"
+  },
+  {
+    "phone": "No.0829",
+    "name": "彼方和歌"
+  },
+  {
+    "phone": "No.2356",
+    "name": "里世界的我"
+  },
+  {
+    "phone": "No.2219",
+    "name": "七夜栗琦不姓朱"
+  },
+  {
+    "phone": "No.5832",
+    "name": "玄岚法師"
+  },
+  {
+    "phone": "No.1550",
+    "name": "staya"
+  },
+  {
+    "phone": "No.5450",
+    "name": "露露緹娅"
+  },
+  {
+    "phone": "No.9498",
+    "name": "朝仓音姬"
+  },
+  {
+    "phone": "No.6912",
+    "name": "东方橘君"
+  },
+  {
+    "phone": "No.1414",
+    "name": "幻月之舞"
+  },
+  {
+    "phone": "No.9994",
+    "name": "六月沐夏w"
+  },
+  {
+    "phone": "No.8350",
+    "name": "推倒不管埋"
+  },
+  {
+    "phone": "No.2392",
+    "name": "约修亚布莱特"
+  },
+  {
+    "phone": "No.1506",
+    "name": "白钟莎罗"
+  },
+  {
+    "phone": "No.4702",
+    "name": "翻滚吧贡丸"
+  },
+  {
+    "phone": "No.7014",
+    "name": "安布雷拉"
+  },
+  {
+    "phone": "No.9099",
+    "name": "钉宫病重症患"
+  },
+  {
+    "phone": "No.4343",
+    "name": "摘花五十铃"
+  },
+  {
+    "phone": "No.6606",
+    "name": "阿SAM"
+  },
+  {
+    "phone": "No.5478",
+    "name": "qoyozy"
+  },
+  {
+    "phone": "No.2292",
+    "name": "沙漠孤魂"
+  },
+  {
+    "phone": "No.2879",
+    "name": "WSDS"
+  },
+  {
+    "phone": "No.6300",
+    "name": "刀子子刀"
+  },
+]
